46af630e98d31e941ee92cf0a16499749031b112
Deepak Choudhari <Deepak.Choudhari@outlook.com>
2017-09-30 07:01:21 -0500

    Initial commit.

---
+++ WeatherDataFunctionApp/WeatherDataService.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace WeatherDataFunctionApp
+{
+    public static class WeatherDataService
+    {
+        [FunctionName("WeatherDataService")]
+        public static HttpResponseMessage Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+
+            return req.CreateResponse(HttpStatusCode.OK, DateTime.Now.ToString());
+
+            /*
+            // parse query parameter
+            string name = req.GetQueryNameValuePairs()
+                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
+                .Value;
+
+            // Get request body
+            dynamic data = await req.Content.ReadAsAsync<object>();
+
+            // Set name to query string or body data
+            name = name ?? data?.name;
+
+            return name == null
+                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
+                : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
+            */
+        }
+    }
+}
